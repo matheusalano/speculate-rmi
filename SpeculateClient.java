bc766729940cab504cba369c08f3ccebd1eb49cc
Matheus Alano <matheus_dias@terceiros.sicredi.com.br>
2019-04-10 21:08:28 -0300

    Começo interface com o usuário.

--- SpeculateClient.java
+++ SpeculateClient.java
@@ -1,66 +1,94 @@
 import java.rmi.Naming;
+import java.util.Scanner;
 
 public class SpeculateClient {
 
     public static void main(String[] args) {
         try {
+            Scanner scan = new Scanner (System.in);  
             SpeculateInterface speculate = (SpeculateInterface) Naming.lookup ("//localhost/Speculate"); 
-            int id = speculate.registraJogador("matheusalano"); System.out.println ("registraJogador01: " + id);
-            System.out.println ("temPartida01: " + speculate.temPartida(id));
+           
+            System.out.println ("Bem-vindo(a) ao Speculate!\n\n");  
 
-            int id2 = speculate.registraJogador("domisobrosa"); System.out.println ("registraJogador02: " + id2);
-            System.out.println ("temPartida01: " + speculate.temPartida(id));
+            //Registro do usuário
+            int id;
+            while (true) {
+                System.out.println ("Para começar, informe um usuário: \n");  
+                String username = scan.nextLine(); 
+            
+                id = speculate.registraJogador(username);
 
-            //JOGADOR 1
-            System.out.println ("defineJogadas01: " + speculate.defineJogadas(id, 5));
-            System.out.println ("jogaDado01: " + speculate.jogaDado(id));
-            System.out.println ("jogaDado01: " + speculate.jogaDado(id));
-            System.out.println ("jogaDado01: " + speculate.jogaDado(id));
-            System.out.println ("jogaDado01: " + speculate.jogaDado(id));
-            System.out.println ("jogaDado01: " + speculate.jogaDado(id));
+                if (id == -1) {
+                    System.out.println ("Usuário já está cadastrado. Tente novamente...\n\n");  
+                    Thread.sleep(1000);
+                } else if (id == -2) {
+                    System.out.println ("Não há partidas no momento. Tente novamente mais tarde...\n\n");  
+                    System.exit(0);
+                } else {
+                    System.out.println ("Obrigado!\n");  
+                    break;
+                }
+            }
+            
+            //Aguardo do segundo jogador
+            System.out.println ("Procurando outro jogador.\n");  
+            System.out.print ("Aguarde...");  
 
-            System.out.println ("obtemTabuleiro01: " + speculate.obtemTabuleiro(id));
-            System.out.println ("obtemNumBolas01: " + speculate.obtemNumBolas(id));
-            System.out.println ("ehMinhaVez01: " + speculate.ehMinhaVez(id));
+            while (true) {
+                int temPartida = speculate.temPartida(id);
 
-            //JOGADOR 2
-            System.out.println ("defineJogadas02: " + speculate.defineJogadas(id2, 5));
-            System.out.println ("jogaDado02: " + speculate.jogaDado(id2));
-            System.out.println ("jogaDado02: " + speculate.jogaDado(id2));
-            System.out.println ("jogaDado02: " + speculate.jogaDado(id2));
-            System.out.println ("jogaDado02: " + speculate.jogaDado(id2));
-            System.out.println ("jogaDado02: " + speculate.jogaDado(id2));
+                if (temPartida == -2) {
+                    System.out.println ("Não encontramos outro jogador. Tente novamente mais tarde...\n\n");  
+                    System.exit(0);
+                } else if (temPartida == -1) {
+                    System.out.println ("Ocorreu um erro inesperado...\n\n");  
+                    System.exit(1);
+                } else if (temPartida == 0) {
+                    System.out.print(".");
+                    Thread.sleep(2000);
+                } else if (temPartida == 1) {
+                    System.out.println ("Encontramos outro jogador e você inicia jogando.");  
+                    System.out.println ("Jogador " + speculate.obtemOponente(id) + "está aguardando sua jogada.\n\n");  
+                    break;
+                } else if (temPartida == 2) {
+                    System.out.println ("Encontramos outro jogador e ele inicia jogando.");  
+                    System.out.println ("Aguarde enquanto " + speculate.obtemOponente(id) + "realiza sua jogada.\n\n");  
+                    break;
+                }
+            }
+            
+            while (true) {
+                int temPartida = speculate.ehMinhaVez(id);
+                
+                if (temPartida == -2) {
+                    System.out.println ("Não encontramos outro jogador. Tente novamente mais tarde...\n\n");  
+                    System.exit(0);
+                } else if (temPartida == -1) {
+                    System.out.println ("Ocorreu um erro inesperado...\n\n");  
+                    System.exit(1);
+                } else if (temPartida == 0) {
+                    System.out.print(".");
+                    Thread.sleep(2000);
+                } else if (temPartida == 2) {
+                    System.out.println("Você é o vencedor. Parabéns!\n");
+                    break;
+                } else if (temPartida == 3) {
+                    System.out.println("Você perdeu.\n");
+                    break;
+                } else if (temPartida == 5) {
+                    System.out.println("Você venceu por WO. Parabéns!\n");
+                    break;
+                } else if (temPartida == 6) {
+                    System.out.println("Você perdeu por WO.\n");
+                    break;
+                } else if (temPartida == 1) {
+                    //tratar jogadas.
+                }
 
-            System.out.println ("obtemTabuleiro02: " + speculate.obtemTabuleiro(id2));
-            System.out.println ("obtemNumBolas02: " + speculate.obtemNumBolas(id2));
-            System.out.println ("ehMinhaVez02: " + speculate.ehMinhaVez(id2));
+            }
 
-            //JOGADOR 1
-            System.out.println ("defineJogadas01: " + speculate.defineJogadas(id, 2));
-            System.out.println ("jogaDado01: " + speculate.jogaDado(id));
-            System.out.println ("jogaDado01: " + speculate.jogaDado(id));
+            scan.close();
 
-            System.out.println ("obtemTabuleiro01: " + speculate.obtemTabuleiro(id));
-            System.out.println ("obtemNumBolas01: " + speculate.obtemNumBolas(id));
-            System.out.println ("ehMinhaVez01: " + speculate.ehMinhaVez(id));
-
-            //JOGADOR 2
-            System.out.println ("defineJogadas02: " + speculate.defineJogadas(id2, 2));
-            System.out.println ("jogaDado02: " + speculate.jogaDado(id2));
-            System.out.println ("jogaDado02: " + speculate.jogaDado(id2));
-
-            System.out.println ("obtemTabuleiro02: " + speculate.obtemTabuleiro(id2));
-            System.out.println ("obtemNumBolas02: " + speculate.obtemNumBolas(id2));
-            System.out.println ("ehMinhaVez02: " + speculate.ehMinhaVez(id2));
-
-            //JOGADOR 1
-            System.out.println ("defineJogadas01: " + speculate.defineJogadas(id, 2));
-            System.out.println ("jogaDado01: " + speculate.jogaDado(id));
-            System.out.println ("jogaDado01: " + speculate.jogaDado(id));
-
-            System.out.println ("obtemTabuleiro01: " + speculate.obtemTabuleiro(id));
-            System.out.println ("obtemNumBolas01: " + speculate.obtemNumBolas(id));
-            System.out.println ("ehMinhaVez01: " + speculate.ehMinhaVez(id));
         } catch (Exception e) {
             System.out.println ("HelloClient failed:"); 
             e.printStackTrace();
